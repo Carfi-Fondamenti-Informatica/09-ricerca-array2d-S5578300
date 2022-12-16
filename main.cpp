623d9b7749a6001b5eba60063e68ee783139b61f
S5578300 <118068983+S5578300@users.noreply.github.com>
2022-12-16 18:50:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+use namespace std;
 
 int main(){
+  char Matrice[10][20];
+  char Matrice2 [20];
+  int pos=0;
+  for (int i=0; i<10; i++){
+    for (int j=0, j<20; j++){
+      Matrice [i][j]='   ';
+      Matrice2 [j]='  ';
+    }
+  }
+  for (int i=0; i<10; i++){
+    cin >> Array[i];
+  }
+  cin>> Matrice2;
+  if (funzione(Array, Array2,pos)){
+    cout << pos;
+  }else{
+    cout << "non presente";
+       }
   return 0;
 }
