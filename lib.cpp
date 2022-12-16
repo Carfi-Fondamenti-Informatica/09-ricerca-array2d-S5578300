3cf35d33c12b1bd584a96c8a0dbe833238676314
S5578300 <118068983+S5578300@users.noreply.github.com>
2022-12-16 18:43:32 +0100

    Create lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#includi "lib.h"
 
+bool funzione (char Array [10][20], char Array2[20], int &pos){
+  for (int i=0; i<10; i++){
+    for (int j=0; j<20; j++){
+      if ( Array2[j] != Array[i][j]){
+        j=20;
+      }else se(j==19){
+        pos =i;
+        restituisci true;
+      }
+    }
+  }
+  returne false;
+}
